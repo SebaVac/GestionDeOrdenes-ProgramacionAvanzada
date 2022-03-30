343eeb4ca34ccc828f520846e98eb3d86f7e2ea9
Seba_vac <80701967+SebaVac@users.noreply.github.com>
2022-03-30 19:26:42 -0300

    Actualización Orden.java

--- ProyectoGestionDeOrdenes/src/main/java/proyectogestiondeordenes/Orden.java
+++ ProyectoGestionDeOrdenes/src/main/java/proyectogestiondeordenes/Orden.java
@@ -5,11 +5,13 @@ import java.io.*;
     public class Orden {
         /*Variables iniciales*/
         private String nombre;
+        private int rut;
         private String servicio;
         
         /*Constructores*/
-        public Orden(String nombre, String servicio) {
+        public Orden(String nombre, int rut, String servicio) {
             this.nombre = nombre;
+            this.rut = rut;
             this.servicio = servicio;
         }
         
@@ -21,6 +23,10 @@ import java.io.*;
             return nombre;
         }
 
+        public int getRut() {
+            return rut;
+        }
+
         public String getServicio() {
             return servicio;
         }
@@ -29,6 +35,10 @@ import java.io.*;
         /*Setter*/
         public void setNombre(String nombre) {
             this.nombre = nombre;
+        }
+
+        public void setRut(int rut) {
+            this.rut = rut;
         }
 
         public void setServicio(String servicio) {
