95e497c10d219b70269283a2a0874248c9285d3f
Seba_vac <80701967+SebaVac@users.noreply.github.com>
2022-03-30 19:07:48 -0300

    Corrección falta de clase main

--- ProyectoGestionDeOrdenes/src/main/java/proyectogestiondeordenes/ProyectoGestionDeOrdenes.java
+++ ProyectoGestionDeOrdenes/src/main/java/proyectogestiondeordenes/ProyectoGestionDeOrdenes.java
@@ -5,9 +5,11 @@ import java.util.*;
    
 public class ProyectoGestionDeOrdenes {
 
+    public static void main (String[] arg) {
     /*Crear variables*/
     
     HashMap mapaDeOrdenes = new HashMap();
     
     //Empresa empresa = new Empresa(,mapaDeOrdenes);
+    }
 }
