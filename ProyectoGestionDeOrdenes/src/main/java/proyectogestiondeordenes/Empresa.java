93f8b511d44bb0b7595940544a45b1fb9f497a90
Acdiel <80924434+Acdiel@users.noreply.github.com>
2022-03-31 22:14:10 -0300

    Update Empresa.java

--- ProyectoGestionDeOrdenes/src/main/java/proyectogestiondeordenes/Empresa.java
+++ ProyectoGestionDeOrdenes/src/main/java/proyectogestiondeordenes/Empresa.java
@@ -5,17 +5,28 @@ import java.io.*;
 import java.util.*;
 
 public class Empresa {
-    /*Variables iniciales*/
+    
+     /*Variables iniciales*/
     private int rut; //key del Hashmap
     private HashMap mapaOrdenes = new HashMap();
+    private ArrayList<Orden> ordenes;
+    
 
     /*Constructores*/
     public Empresa(int rut, HashMap mapaOrdenes) {
         this.rut = rut;
         this.mapaOrdenes = mapaOrdenes;
+        ordenes = new ArrayList();
     }
     
-
+    public Empresa(int rut, ArrayList aa){
+        this.rut = rut;
+        // libros = aa // esto esta MALO
+        ordenes = new ArrayList();
+        ordenes.addAll(aa);
+    }
+    
+ 
     //Metodos
     
     /*Setter*/
@@ -26,8 +37,7 @@ public class Empresa {
     public void setMapaOrdenes(HashMap mapaOrdenes) {
         this.mapaOrdenes = mapaOrdenes;
     }
-
-
+    
     /*Getter*/
     public int getRut() {
         return rut;
@@ -36,4 +46,11 @@ public class Empresa {
     public HashMap getMapaOrdenes() {
         return mapaOrdenes;
     }
+    
+    public void agregarOrdenes(Orden oo){
+        ordenes.add(oo);
+    }
+    
+    
 }
+
