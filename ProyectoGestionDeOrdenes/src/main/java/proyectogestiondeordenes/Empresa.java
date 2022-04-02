24461999826c0b39cc3360b7a2911c8ed401d31a
Acdiel <80924434+Acdiel@users.noreply.github.com>
2022-04-01 21:18:12 -0300

    Se agregan las Sobrecargas

--- ProyectoGestionDeOrdenes/src/main/java/proyectogestiondeordenes/Empresa.java
+++ ProyectoGestionDeOrdenes/src/main/java/proyectogestiondeordenes/Empresa.java
@@ -1,56 +1,61 @@
 
 package proyectogestiondeordenes;
 
-import java.io.*;
 import java.util.*;
 
 public class Empresa {
     
      /*Variables iniciales*/
     private int rut; //key del Hashmap
-    private HashMap mapaOrdenes = new HashMap();
+    private String nombre;
     private ArrayList<Orden> ordenes;
     
 
     /*Constructores*/
-    public Empresa(int rut, HashMap mapaOrdenes) {
+    public Empresa(int rut, String nombre) {
         this.rut = rut;
-        this.mapaOrdenes = mapaOrdenes;
+        this.nombre = nombre;
         ordenes = new ArrayList();
     }
     
-    public Empresa(int rut, ArrayList aa){
+    public Empresa(int rut, String nombre, ArrayList aa){
         this.rut = rut;
+        this.nombre = nombre;
         // libros = aa // esto esta MALO
         ordenes = new ArrayList();
         ordenes.addAll(aa);
     }
     
- 
-    //Metodos
-    
     /*Setter*/
     public void setRut(int rut) {
         this.rut = rut;
     }
-
-    public void setMapaOrdenes(HashMap mapaOrdenes) {
-        this.mapaOrdenes = mapaOrdenes;
+  
+    public String getNombre(){
+        return nombre;
+    }
+    
+    public void setNombre(String nombre){
+        this.nombre = nombre;
     }
     
     /*Getter*/
     public int getRut() {
         return rut;
     }
-
-    public HashMap getMapaOrdenes() {
-        return mapaOrdenes;
-    }
     
-    public void agregarOrdenes(Orden oo){
+    public void agregarOrden(Orden oo){
         ordenes.add(oo);
     }
     
+    // SOBRECARGAS
+    
+    public void buscarOrden(int rut){
+        System.out.println(rut);
+    }
+    
+    public void buscarOrden(String nombre){
+        System.out.println(nombre);
+    }
     
 }
-
