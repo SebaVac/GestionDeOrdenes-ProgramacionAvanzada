00a9e7baa8958797c74f13a48e68322a101c97ae
Acdiel <80924434+Acdiel@users.noreply.github.com>
2022-04-01 21:19:22 -0300

    Se agregar la funcion MostrarMapa

--- ProyectoGestionDeOrdenes/src/main/java/proyectogestiondeordenes/Orden.java
+++ ProyectoGestionDeOrdenes/src/main/java/proyectogestiondeordenes/Orden.java
@@ -44,5 +44,25 @@ import java.io.*;
         public void setServicio(String servicio) {
             this.servicio = servicio;
         }
+        
+        public void mostrarMapa(HashMap Map){
+            
+        Map<Integer,Orden> map=new HashMap<Integer,Orden>();  
+         
+        Orden o1 = new Orden("Acdiel", 20906578, "Arreglo Pantalla");
+        Orden o2 = new Orden("Seba", 20957841, "Cargador");
+        
+        map.put(1,o1);
+        map.put(2,o2);  
+        
+        //Traversing map  
+        for(Map.Entry<Integer, Orden> entry:map.entrySet()){    
+        int key=entry.getKey();  
+        Orden o=entry.getValue();  
+        System.out.println(key+" Details:");  
+        System.out.println(o.nombre+" "+o.rut+" "+o.servicio); 
+        
+        }
+        }
 
     }
