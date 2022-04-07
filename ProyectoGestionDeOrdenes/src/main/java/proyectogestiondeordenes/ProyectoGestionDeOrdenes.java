566321c61b3fa0f1f1822ce3631110b0526ae8be
Seba_vac <80701967+SebaVac@users.noreply.github.com>
2022-04-07 15:43:37 -0400

    Update ProyectoGestionDeOrdenes.java

--- ProyectoGestionDeOrdenes/src/main/java/proyectogestiondeordenes/ProyectoGestionDeOrdenes.java
+++ ProyectoGestionDeOrdenes/src/main/java/proyectogestiondeordenes/ProyectoGestionDeOrdenes.java
@@ -13,41 +13,47 @@ public class ProyectoGestionDeOrdenes {
         
     /*Menu de opciones*/
     System.out.println("Menu\n");
-    System.out.println("1.- Ingresar Archivo\n");
-    System.out.println("2.- Eliminar Orden\n");
-    System.out.println("3.- Buscar Orden\n");
-    System.out.println("4.- Mostrar Ordenes\n");
+    System.out.println("1.- Ingresar Datos Manualmente\n");
+    System.out.println("2.- Ingresar Archivo\n");
+    System.out.println("3.- Eliminar Orden\n");
+    System.out.println("4.- Buscar Orden\n");
+    System.out.println("5.- Mostrar Ordenes\n");
     System.out.println("Ingresar opcion correspondiente:");
     
     /*se verifica que la opcion sea valida*/
     do{
         opcion = usuario.readLine();
         seleccion = Integer.parseInt(opcion);
-        if((seleccion > 5) || (seleccion < 1)){
+        if((seleccion > 6) || (seleccion < 1)){
             System.out.println("Opcion no valida, intente nuevamente: ");
         }
-    }while((seleccion > 5) || (seleccion < 1));
+    }while((seleccion > 6) || (seleccion < 1));
     
     /*se llama la funcion segun corresponda*/
     switch(seleccion){
         case 1:
-            System.out.println("Ingresar nombre del archivo: ");
+            System.out.println("Ingresar Nombre del usuario: ");
+          
             
             break;
             
         case 2:
-            //funcion que tiene como parametro el rut y el identificador para buscar la orden a eliminar
+            System.out.println("Ingresar nombre del archivo: ");
             break;
             
         case 3:
+            //funcion que tiene como parametro el rut y el identificador para buscar la orden a eliminar
+            break;
+            
+        case 4:
             System.out.println("Ingresar Rut de la persona");
             opcion = usuario.readLine();
             //funcion que tiene como parametro el rut de la persona y busca la orden correspondiente
             break;
-            
-        case 4:
-            //Se muestran TODAS las ordenes
-            break;
+          
+       case 5:
+          //Se muestran TODAS las ordenes
+          break;
     }
     
     }
