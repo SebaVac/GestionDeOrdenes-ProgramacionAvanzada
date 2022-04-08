671a83a6e02f7d7d19a1080034a088799376be23
Seba_vac <80701967+SebaVac@users.noreply.github.com>
2022-04-08 19:02:27 -0400

    Se agregan las funciones para el menu

--- ProyectoGestionDeOrdenes/src/main/java/proyectogestiondeordenes/ProyectoGestionDeOrdenes.java
+++ ProyectoGestionDeOrdenes/src/main/java/proyectogestiondeordenes/ProyectoGestionDeOrdenes.java
@@ -8,12 +8,12 @@ public class ProyectoGestionDeOrdenes {
     public static void main (String[] arg) throws FileNotFoundException, IOException {
         
         int seleccion;
-        Empresa empresa = null;
+        BufferedReader usuario = new BufferedReader(new InputStreamReader(System.in));
         
     /*Menu de opciones*/
     do{
-        seleccion = mostrarMenu();
-    /*se llama la funcion segun corresponda*/
+        seleccion = mostrarMenu(usuario);
+
     switch(seleccion){
         case 1:
            
@@ -42,11 +42,14 @@ public class ProyectoGestionDeOrdenes {
     
     }
     
-    public static int mostrarMenu() throws FileNotFoundException, IOException {
+    /*
+    *   FUNCIONES
+    */
+    
+    public static int mostrarMenu(BufferedReader usuario) throws FileNotFoundException, IOException {
         
         String opcion;
         int seleccion;
-        BufferedReader usuario = new BufferedReader(new InputStreamReader(System.in));
         
         System.out.println("Menu\n");
         System.out.println("1.- Ingresar Datos Manualmente\n");
@@ -67,7 +70,45 @@ public class ProyectoGestionDeOrdenes {
     }while((seleccion > 7) || (seleccion < 1));
     
     return seleccion;
+    
     }
 
-}
+    
+    @SuppressWarnings("null")
+    public static void agregarPersona(BufferedReader usuario) throws FileNotFoundException, IOException{
+        
+        Orden orden = null;
+        Persona persona = null;
+        
+        String nombreUsuario;
+        int rutUsuario;
+        String servicio;
 
+        String opcion;
+        
+        System.out.println("Ingresar Nombre del usuario: ");
+        nombreUsuario = usuario.readLine();
+        
+        /*se ingresan a las clases*/
+        orden.setNombre(nombreUsuario);
+        persona.setNombre(nombreUsuario);
+
+        System.out.println("\nIngresar Rut del usuario(sin puntos ni digito verificador): ");
+        opcion = usuario.readLine();
+        rutUsuario = Integer.parseInt(opcion);
+        
+        /*se ingresan a las clases*/
+        orden.setRut(rutUsuario);
+        persona.setRut(rutUsuario);
+
+        System.out.println("\nIngresar Servicio: ");
+        servicio = usuario.readLine();
+        
+        /*se ingresa a la clase orden*/
+        orden.setServicio(servicio);
+        
+        /*agregar al mapa*/
+        
+        
+    }
+}
