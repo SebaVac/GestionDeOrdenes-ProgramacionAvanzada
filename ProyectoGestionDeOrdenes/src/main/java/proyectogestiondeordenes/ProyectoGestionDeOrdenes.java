f345c4c22e01e16325ab677f0586f1e4ad8c0bd7
Seba_vac <80701967+SebaVac@users.noreply.github.com>
2022-04-08 18:05:18 -0400

    Update ProyectoGestionDeOrdenes.java

--- ProyectoGestionDeOrdenes/src/main/java/proyectogestiondeordenes/ProyectoGestionDeOrdenes.java
+++ ProyectoGestionDeOrdenes/src/main/java/proyectogestiondeordenes/ProyectoGestionDeOrdenes.java
@@ -6,35 +6,17 @@ import java.util.*;
 public class ProyectoGestionDeOrdenes {
 
     public static void main (String[] arg) throws FileNotFoundException, IOException {
-    
-        String opcion;
+        
         int seleccion;
-        BufferedReader usuario = new BufferedReader(new InputStreamReader(System.in));
+        Empresa empresa = null;
         
     /*Menu de opciones*/
-    System.out.println("Menu\n");
-    System.out.println("1.- Ingresar Datos Manualmente\n");
-    System.out.println("2.- Ingresar Archivo\n");
-    System.out.println("3.- Eliminar Orden\n");
-    System.out.println("4.- Buscar Orden\n");
-    System.out.println("5.- Mostrar Ordenes\n");
-    System.out.println("Ingresar opcion correspondiente:");
-    
-    /*se verifica que la opcion sea valida*/
     do{
-        opcion = usuario.readLine();
-        seleccion = Integer.parseInt(opcion);
-        if((seleccion > 6) || (seleccion < 1)){
-            System.out.println("Opcion no valida, intente nuevamente: ");
-        }
-    }while((seleccion > 6) || (seleccion < 1));
-    
+        seleccion = mostrarMenu();
     /*se llama la funcion segun corresponda*/
     switch(seleccion){
         case 1:
-            System.out.println("Ingresar Nombre del usuario: ");
-          
-            
+           
             break;
             
         case 2:
@@ -47,18 +29,45 @@ public class ProyectoGestionDeOrdenes {
             
         case 4:
             System.out.println("Ingresar Rut de la persona");
-            opcion = usuario.readLine();
+            //opcion = usuario.readLine();
             //funcion que tiene como parametro el rut de la persona y busca la orden correspondiente
             break;
           
        case 5:
           //Se muestran TODAS las ordenes
-            System.out.println("Se muestra TODAS las ordenes : \n");
-            ProyectoGestionDeOrdenes.mostrarOrdenes();
-            break;
           break;
     }
     
+    }while(seleccion != 6);
+    
     }
+    
+    public static int mostrarMenu() throws FileNotFoundException, IOException {
+        
+        String opcion;
+        int seleccion;
+        BufferedReader usuario = new BufferedReader(new InputStreamReader(System.in));
+        
+        System.out.println("Menu\n");
+        System.out.println("1.- Ingresar Datos Manualmente\n");
+        System.out.println("2.- Ingresar Archivo\n");
+        System.out.println("3.- Eliminar Orden\n");
+        System.out.println("4.- Buscar Orden\n");
+        System.out.println("5.- Mostrar Ordenes\n");
+        System.out.println("6.- Salir");
+        System.out.println("Ingresar opcion correspondiente:");
+
+    /*se verifica que la opcion sea valida*/
+    do{
+        opcion = usuario.readLine();
+        seleccion = Integer.parseInt(opcion);
+        if((seleccion > 7) || (seleccion < 1)){
+            System.out.println("Opcion no valida, intente nuevamente: ");
+        }
+    }while((seleccion > 7) || (seleccion < 1));
+    
+    return seleccion;
+    }
+
 }
 
