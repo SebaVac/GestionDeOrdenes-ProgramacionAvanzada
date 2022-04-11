3ba45a2572e6a562f592014b3601083ec8bf7d4d
Acdiel <80924434+Acdiel@users.noreply.github.com>
2022-04-10 22:12:17 -0400

    EP3

--- ProyectoGestionDeOrdenes/src/main/java/proyectogestiondeordenes/Orden.java
+++ ProyectoGestionDeOrdenes/src/main/java/proyectogestiondeordenes/Orden.java
@@ -1,29 +1,51 @@
-package proyectogestiondeordenes;
+package proyectoremasterizado;
 
 import java.io.*;
+import java.util.ArrayList;
 
-    public class Orden {
+public class Orden {
+    
         /*Variables iniciales*/
         private String nombre;
-        private int rut;
+        private String rut;
         private String servicio;
+        public ArrayList<Orden> listaOrdenes;
         
         /*Constructores*/
-        public Orden(String nombre, int rut, String servicio) {
+        public Orden(String nombre, String rut, String servicio) {
             this.nombre = nombre;
             this.rut = rut;
             this.servicio = servicio;
+            this.listaOrdenes = new ArrayList<>();
         }
         
+        public void mostrarOrden(){
+        System.out.println("El RUT "+this.rut+" es de la persona "+this.nombre+", el cual necesita un servicio de "+this.servicio+".");
+    }
         
-        //Metodos
-
+        public void agregarOrden(Orden o){
+        this.listaOrdenes.add(o);
+        System.out.println("Orden Agregada");
+    }
+       public void mostrarOrdenes(){
+        for(int i = 0; i<this.listaOrdenes.size(); i++){
+            this.listaOrdenes.get(i).mostrarOrden();
+        }
+    }
+        public void mostrarServicios(String nombre){                            //para la busqueda
+        for(int i = 0; i<this.listaOrdenes.size(); i++){
+            if (this.listaOrdenes.get(i).getNombre().equals(nombre)){
+                this.listaOrdenes.get(i).mostrarOrden();
+            }
+        }
+    }
+       
         /*Getter*/
         public String getNombre() {
             return nombre;
         }
 
-        public int getRut() {
+        public String getRut() {
             return rut;
         }
 
@@ -37,7 +59,7 @@ import java.io.*;
             this.nombre = nombre;
         }
 
-        public void setRut(int rut) {
+        public void setRut(String rut) {
             this.rut = rut;
         }
 
@@ -45,42 +67,15 @@ import java.io.*;
             this.servicio = servicio;
         }
         
-        public void mostrarMapa(HashMap Map){
-            
-        Map<Integer,Orden> map=new HashMap<Integer,Orden>();  
-         
-        Orden o1 = new Orden("Acdiel", 20906578, "Arreglo Pantalla");
-        Orden o2 = new Orden("Seba", 20957841, "Cargador");
-        
-        map.put(1,o1);
-        map.put(2,o2);  
-        
-        //Traversing map  
-        for(Map.Entry<Integer, Orden> entry:map.entrySet()){    
-        int key=entry.getKey();  
-        Orden o=entry.getValue();  
-        System.out.println(key+" Details:");  
-        System.out.println(o.nombre+" "+o.rut+" "+o.servicio); 
-        
-        }
-        }
-        
-        
-        public void mostrarOrdenes(String nombre){    
-       
-        }
-        public void eliminarMapa(){
-            
-            
-            
-        }
-        
-        public void eliminarOrden(int rut){
-        System.out.println(rut);
-    }
-    
-        public void eliminarOrden(String nombre){
-        System.out.println(nombre);
+        public ArrayList<Orden> getListaOrdenes() {
+        return listaOrdenes;
     }
 
+        public void setListaProductos(ArrayList<Orden> listaOrdenes) {
+        this.listaOrdenes = listaOrdenes;
     }
+        
+   
+
+    }
+    
