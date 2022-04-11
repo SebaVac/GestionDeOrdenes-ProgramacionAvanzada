2707505d74a9723bd9b6248b07707e4c8ce4a3ed
Acdiel <80924434+Acdiel@users.noreply.github.com>
2022-04-10 22:13:20 -0400

    EP3

---
+++ ProyectoGestionDeOrdenes/src/main/java/proyectogestiondeordenes/Bienvenida.java
@@ -0,0 +1,15 @@
+
+package proyectoremasterizado;
+
+
+public class Bienvenida {
+    public static void main(String[] args) {
+        System.out.println("-------------");
+        System.out.println("| BIENVENIDO |");
+        System.out.println("-------------");
+        System.out.println("¿Que desea realizar? :");
+        Menu m = new Menu ();
+        m.metodMenu(1);
+        
+    }    
+}
