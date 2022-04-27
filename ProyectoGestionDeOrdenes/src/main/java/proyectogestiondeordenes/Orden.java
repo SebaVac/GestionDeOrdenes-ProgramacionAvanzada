e1306e65169cb4c2c0e5e37d64cd12a33bd6291f
Seba_vac <80701967+SebaVac@users.noreply.github.com>
2022-04-27 16:53:05 -0400

    Correccion Orden.java

--- ProyectoGestionDeOrdenes/src/main/java/proyectogestiondeordenes/Orden.java
+++ ProyectoGestionDeOrdenes/src/main/java/proyectogestiondeordenes/Orden.java
@@ -1,81 +1,32 @@
-package proyectoremasterizado;
-
-import java.io.*;
-import java.util.ArrayList;
+package proyectogestiondeordenes;
 
 public class Orden {
-    
-        /*Variables iniciales*/
-        private String nombre;
-        private String rut;
-        private String servicio;
-        public ArrayList<Orden> listaOrdenes;
-        
-        /*Constructores*/
-        public Orden(String nombre, String rut, String servicio) {
-            this.nombre = nombre;
-            this.rut = rut;
-            this.servicio = servicio;
-            this.listaOrdenes = new ArrayList<>();
-        }
-        
-        public void mostrarOrden(){
-        System.out.println("El RUT "+this.rut+" es de la persona "+this.nombre+", el cual necesita un servicio de "+this.servicio+".");
-    }
-        
-        public void agregarOrden(Orden o){
-        this.listaOrdenes.add(o);
-        System.out.println("Orden Agregada");
-    }
-       public void mostrarOrdenes(){
-        for(int i = 0; i<this.listaOrdenes.size(); i++){
-            this.listaOrdenes.get(i).mostrarOrden();
-        }
-    }
-        public void mostrarServicios(String nombre){                            //para la busqueda
-        for(int i = 0; i<this.listaOrdenes.size(); i++){
-            if (this.listaOrdenes.get(i).getNombre().equals(nombre)){
-                this.listaOrdenes.get(i).mostrarOrden();
-            }
-        }
-    }
-       
-        /*Getter*/
-        public String getNombre() {
-            return nombre;
-        }
 
-        public String getRut() {
-            return rut;
-        }
+    private String rut;
+    private String Servicio;
 
-        public String getServicio() {
-            return servicio;
-        }
-
-
-        /*Setter*/
-        public void setNombre(String nombre) {
-            this.nombre = nombre;
-        }
-
-        public void setRut(String rut) {
-            this.rut = rut;
-        }
-
-        public void setServicio(String servicio) {
-            this.servicio = servicio;
-        }
-        
-        public ArrayList<Orden> getListaOrdenes() {
-        return listaOrdenes;
+    /*Constructor*/
+    public Orden(String rut, String Servicio) {
+        this.rut = rut;
+        this.Servicio = Servicio;
     }
 
-        public void setListaProductos(ArrayList<Orden> listaOrdenes) {
-        this.listaOrdenes = listaOrdenes;
+    /*Setters*/
+    public void setRut(String rut) {
+        this.rut = rut;
     }
-        
-   
 
+    public void setServicio(String Servicio) {
+        this.Servicio = Servicio;
     }
-    
+
+    /*Getters*/
+    public String getRut() {
+        return rut;
+    }
+
+    public String getServicio() {
+        return Servicio;
+    }
+
+}
